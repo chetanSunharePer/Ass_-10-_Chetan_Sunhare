03db97ac84d6e6d3d411d0e2d7df9dabf76cf846
ChetanSunhare <c.sunhare@gmail.com>
2021-03-13 14:16:09 +0530

    Contact.java file is created in beans package.

---
+++ Ass_(10)_Chetan_Sunhare/src/com/psl/training/assigment/beans/Contact.java
@@ -0,0 +1,32 @@
+package com.psl.training.assigment.beans;
+
+public class Contact {
+	private int contactID;
+	private String contactName;
+	private String emailAddress;
+	public Contact(int contactID,String contactName,String emailAddress) {
+		this.contactID = contactID;
+		this.contactName = contactName;
+		this.emailAddress = emailAddress;
+	}
+	
+	public int getContactID() {
+		return contactID;
+	}
+	public void setContactID(int contactID) {
+		this.contactID = contactID;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	
+}
